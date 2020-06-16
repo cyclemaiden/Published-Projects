ac28e15a2f113d3766155db97ea1f6a587fe9ccc
Lyn Phan <22333215+cyclemaiden@users.noreply.github.com>
2020-06-15 22:13:31 -0400

    Removed old broken code from last commit

--- Tabletop Dice Roller/Source Code/stringparser.py
+++ Tabletop Dice Roller/Source Code/stringparser.py
@@ -14,11 +14,6 @@ def parse(inputString):
     for i in range(len(quantitiesAndSizes)):
         if quantitiesAndSizes[i][0] == '':
             quantitiesAndSizes[i][0] = '1'
-
-    # This block prevents a blank input from producing 1 as a result from
-    # the above block.
-    if len(quantitiesAndSizes) == 1 and len(quantitiesAndSizes[0]) == 1:
-        quantitiesAndSizes[0][0] == '0'
     
     return(quantitiesAndSizes)
     
